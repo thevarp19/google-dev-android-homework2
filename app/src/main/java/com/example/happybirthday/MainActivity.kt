36b6cd438280eb06b627f961ed012e6fefac0c0d
Yerlan Jumabayev <thevarp18@gmail.com>
2023-10-13 15:48:37 +0600

    add another text element

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -30,12 +30,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Text(
         text = message,
         fontSize = 100.sp,
         lineHeight = 116.sp,
     )
+    Text(
+        text = from,
+        fontSize = 36.sp
+
+    )
+
 }
 
 
@@ -43,7 +49,8 @@ fun GreetingText(message: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Sam!")
+        GreetingText(message = "Happy Birthday Sam!", from ="From Emma")
+
     }
 }
 
