a61cfa8c80bc83da1391e05ade0fddaa568fd5e9
Yerlan Jumabayev <thevarp18@gmail.com>
2023-10-13 15:53:18 +0600

    Arrange the text elements in a column

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -32,7 +33,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Row {
+    Column(modifier = modifier) {
+
     Text(
         text = message,
         fontSize = 30.sp,
